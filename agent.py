70b33c679ecbf1e75195d0f8ee7f0e42d83d90ae
suryanshp1 <suryanshp1@gmail.com>
2026-09-17 07:47:33 +0530

    feat: env block late injection

--- agent.py
+++ agent.py
@@ -1,6 +1,7 @@
 import json
 
 from llm import SYSTEM_PROMPT, call_llm
+from context import reminder
 from tools import TOOLS
 from ui import ui
 
@@ -17,7 +18,7 @@ while True:
 
     while True:
         with ui.working():
-            message, usage = call_llm(messages)
+            message, usage = call_llm(messages + [reminder()])
 
         messages.append(message.model_dump(exclude_none=True))
         ui.usage(usage)
